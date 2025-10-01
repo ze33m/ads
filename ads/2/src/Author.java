7cd294880df9c07dfde181410ce8d5a08d9989a1
Maxim <m4xim.zemskov@gmail.com>
2025-10-01 09:19:30 +0300

    2 prac

---
+++ ads/2/src/Author.java
@@ -0,0 +1,38 @@
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String name, String email, char gender)
+    {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    
+    public String getNames()
+    {
+        return this.name;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public char getGender() 
+    {
+        return gender;
+    }
+
+    public String toString()
+    {
+        return "Имя: " + this.name + "\temail: " + this.email + "\tгендер: " + this.gender;
+    }
+
+}
---
+++ ads/2/src/TestAuthor.java
@@ -0,0 +1,7 @@
+public class TestAuthor {
+    public static void main(String[] args)
+    {
+        Author author = new Author("максон", "m4xim.zemskov@gmail.com", 'м'); 
+        System.out.println(author.toString());
+    }
+}
