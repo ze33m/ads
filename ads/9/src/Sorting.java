1665a1b062de04a768dbd1be9511636a9c5452fe
Maxim <m4xim.zemskov@gmail.com>
2025-10-08 22:25:14 +0300

    9

--- ads/9/src/Sorting.java
+++ ads/9/src/Sorting.java
@@ -1,15 +1,15 @@
 public class Sorting {
-public static void selectionSort (Comparable[] list){
-int min;
-Comparable temp;
-for (int index = 0; index < list.length-1; index++){
-min = index;
-for (int scan = index+1; scan < list.length; scan++)
- if (list[scan].compareTo(list[min]) < 0){
-min = scan;
- temp = list[min];
- list[min] = list[index];
- list[index] = temp;
- }
- }
+    public static void sort(Comparable[] list) {
+        for (int i = 1; i < list.length; i++) {
+            Comparable key = list[i];
+            int j = i - 1;
+
+
+            while (j >= 0 && list[j].compareTo(key) > 0) {
+                list[j + 1] = list[j];
+                j--;
+            }
+            list[j + 1] = key;
+        }
+    }
 }
